7b709d5b658b065aa08ef0397f67edd00ee2d1ed
axatrikx <axatrikx@gmail.com>
2016-09-16 21:22:17 +0530

    Created basic structure

---
+++ main.js
@@ -0,0 +1,19 @@
+const electron = require('electron')
+
+const {app, BrowserWindow, Menu} = electron
+
+const mTemplate = require('./js/menuTemplate.js')
+
+app.on('ready', () => {
+	let win = new BrowserWindow({
+		width : 800,
+		height: 600,
+		frame : false
+	})
+
+	win.loadURL(`file://${__dirname}/index.html`)
+	win.webContents.openDevTools()
+})
+
+const menu = Menu.buildFromTemplate(mTemplate.template)
+Menu.setApplicationMenu(menu)
